6e983476cd7a9aa3c00bd68a9d9c7cbaffd9dd8a
surethingbro <leobretonb9@gmail.com>
2026-04-13 21:51:15 -0500

    logo test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-
+import logo from "/header-logo.png"
 
 export const Header = () => {
   return (
     <nav className="bg-primarylight shadow-lg flex items-center justify-between py-3 sm:py-5 sm:px-10 px-4 fixed top-0 left-0 w-full">
-            <img className="w-[4.8rem] sm:w-[10rem] md:w-[10rem] lg:w-[13.75rem]" src="/header-logo.png" alt="Logo"/>
+            <img className="w-[4.8rem] sm:w-[10rem] md:w-[10rem] lg:w-[13.75rem]" src={logo} alt="Logo"/>
 
             <div className="flex items-center text-black ">
               <ul className="list-none flex items-center md:gap-x-6 lg:gap-x-8 xl:gap-x-15">
