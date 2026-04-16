9fdc8eb6a1ec7c00fd7c12e44166874c526d3609
surethingbro <leobretonb9@gmail.com>
2026-04-15 21:13:05 -0500

    avances inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -3,18 +3,18 @@ import vid from '../images/Intro-1.mp4'
 
 export const Inicio = () => {
     return (
-        <section id="inicio" className='flex flex-row '>
-            <div className="bg-red-900 px-5 text-center py-5 ">
-                <h1 className='text-sm font-mont-medium '>Más que estética, <br/>
-                <span className=" bg-red-400 font-bold px-3">es confianza.</span> </h1>
+        <section id="inicio" className='flex flex-row  '>
+            <div className=" px-5 text-center py-5 ">
+                <h1 className='text-sm font-mont-medium text-white'>Más que estética, <br/>
+                <span className=" font-bold px-3">es confianza.</span> </h1>
                 
-                <p className='text-[0.5rem] bg-blue-400 mt-2 px-3 w-fit px-2'>
+                <p className='font-mont-regular text-white text-[0.5rem] mt-2 px-3 w-fit px-2'>
                     Recupera tu seguridad con tratamientos pensados para ti 
                 </p>
 
             </div>
 
-            <video autoPlay loop muted className='bg-blue-400 rounded-[15px] py-2 w-50 h-50'>
+            <video autoPlay loop muted className='object-cover rounded-[15px] shadow-[0_3px_8px_rgba(0,0,0,0.15)] py-2 px-2 w-50 h-50'>
                 <source src={vid} type="video/mp4" />
                 Lo sentimos, no se puede mostrar el video en este momento.
             </video>
